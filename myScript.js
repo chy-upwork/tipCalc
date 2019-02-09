25394f9a336e1643fdab5d5fcae46693fa66c69d
Mehedi Hossain Chowdhury <47299659+chy-upwork@users.noreply.github.com>
2019-02-09 21:57:10 +0600

    Web app tipCalc ver 1.0

---
+++ myScript.js
@@ -0,0 +1,63 @@
+//script.js
+
+//Custom function
+
+function calculateTip() {
+	
+	//Store the data of inputs
+	
+	var billAmount = document.getElementById("billAmount").value;
+	
+	var serviceQuality = document.getElementById("serviceQuality").value;
+	
+	var numPeople = document.getElementById("numPeople").value;
+	
+	//Quick validation
+	
+	if(billAmount === "" || serviceQuality == 0){
+		
+		window.alert("Please enter some values to get this baby up and running!!!");
+		return;
+	}
+	
+	//check to see if this input is empty or less than or equal to 1
+	
+	if(numPeople ==="" || numPeople <= 1){
+		
+		numPeople = 1;
+	
+	document.getElementById("each").style.display ="none"; 
+	
+	}
+	
+	else {
+		
+		document.getElementById("each").style.display="block";
+	}
+	
+	//do some math
+	
+	var total = (billAmount * serviceQuality)/ numPeople;
+	
+	total = Math.round(total * 100) / 100;
+	total = total.toFixed(2);
+	
+	//Display the tip!
+	
+	document.getElementById("totalTip").style.display= "block";
+	
+	document.getElementById("tip").innerHTML = total;
+	
+}
+
+//Hide the tip amount on load
+
+document.getElementById("totalTip").style.display= "none";
+document.getElementById("each").style.display= "none";
+
+//Click the button to load custom function
+
+document.getElementById("calculate").onclick = function() { calculateTip(); };
+
+
+
